8213b7f7abd424d9a1df562083d4c57852a24b67
david-lafontant <lafontant.david@gmail.com>
2022-04-29 10:12:59 -0600

    Add Solver class and methods

---
+++ lib/solver.rb
@@ -0,0 +1,25 @@
+class Solver
+    def factorial(num)
+      return 1 if [0, 1].include?(num)
+  
+      raise StandardError if num.negative?
+  
+      num * factorial(num - 1)
+    end
+  
+    def reverse(word)
+      word.reverse
+    end
+  
+    def fizzbuz(num)
+      if (num % 3).zero? && (num % 5).zero?
+        'fizzbuzz'
+      elsif (num % 3).zero?
+        'fizz'
+      elsif (num% 5).zero?
+        'buzz'
+      else
+        num.to_s
+      end
+    end
+  end
